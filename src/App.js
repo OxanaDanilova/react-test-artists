75e18cbfbcdff02ba2a9ff9a68a1712854e77b1c
Oxana Danilova <oxanadanilova11@gmail.com>
2022-07-07 15:07:46 +0200

    feat: use array to render block with info

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
+import { data } from "./info.js";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  const data = {
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Bob_Dylan_-_Azkena_Rock_Festival_2010_2.jpg/800px-Bob_Dylan_-_Azkena_Rock_Festival_2010_2.jpg",
-    cardTitle: "Bob Dylan",
-    cardDescription:
-      "Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.",
-    button: {
-      url: "https://en.wikipedia.org/wiki/Bob_Dylan",
-      label: "Go to wikipedia",
-    },
-  };
-
-  return (
-    <div className="card m-5 mx-auto" style={{ width: "300px" }}>
-      <img className="card-img-top" src={data.image} alt="Bob Dylan" />
-
-      <div className="card-body">
-        <h5 className="card-title">{data.cardTitle}</h5>
-        <p className="card-text">{data.cardDescription}</p>
-        <a href={data.button.url} className="btn btn-primary">
-          {data.button.label}
-        </a>
+  const infoSection = data.map((person, index) => {
+    return (
+      <div key={index} className="card m-5 mx-auto" style={{ width: "300px" }}>
+        <img className="card-img-top" src={person.image} alt="Bob Dylan" />
+        <div className="card-body">
+          <h5 className="card-title">{person.cardTitle}</h5>
+          <p className="card-text">{person.cardDescription}</p>
+          <a href={person.button.url} className="btn btn-primary">
+            {person.button.label}
+          </a>
+        </div>
       </div>
-    </div>
-  );
+    );
+  });
+  return <div>{infoSection}</div>;
 }
 
 export default App;
---
+++ src/info.js
@@ -0,0 +1,35 @@
+export const data = [
+  {
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Bob_Dylan_-_Azkena_Rock_Festival_2010_2.jpg/800px-Bob_Dylan_-_Azkena_Rock_Festival_2010_2.jpg",
+    cardTitle: "Bob Dylan",
+    cardDescription:
+      "Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter, author, and artist who has been an influential figure in popular music and culture for more than five decades.",
+    button: {
+      url: "https://en.wikipedia.org/wiki/Bob_Dylan",
+      label: "Go to wikipedia",
+    },
+  },
+  {
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Paul_McCartney_black_and_white_2010.jpg/220px-Paul_McCartney_black_and_white_2010.jpg",
+    cardTitle: "Paul McCartney",
+    cardDescription:
+      "Sir James Paul McCartney CH MBE (born 18 June 1942) is an English singer, songwriter and musician who gained worldwide fame as co-lead vocalist, co-songwriter and bassist for the Beatles.",
+    button: {
+      url: "https://en.wikipedia.org/wiki/Paul_McCartney",
+      label: "Go to wikipedia",
+    },
+  },
+  {
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Jimi_Hendrix_1967.png/220px-Jimi_Hendrix_1967.png",
+    cardTitle: "Jimi Hendrix",
+    cardDescription:
+      "James Marshall 'Jimi' Hendrix (born Johnny Allen Hendrix; November 27, 1942 – September 18, 1970) was an American guitarist, singer, and songwriter. Although his mainstream career spanned only four years, he is widely regarded as one of the most influential electric guitarists in the history of popular music, and one of the most celebrated musicians of the 20th century. ",
+    button: {
+      url: "https://en.wikipedia.org/wiki/Jimi_Hendrix",
+      label: "Go to wikipedia",
+    },
+  },
+];
