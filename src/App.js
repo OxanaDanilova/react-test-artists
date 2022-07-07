3d1a0321fba2d5e9966cec63d125d00668be770c
Oxana Danilova <oxanadanilova11@gmail.com>
2022-07-07 16:16:11 +0200

    feat: add style from bootstrap

--- src/App.js
+++ src/App.js
@@ -4,8 +4,21 @@ import "bootstrap/dist/css/bootstrap.css";
 function App() {
   const infoSection = data.map((person, index) => {
     return (
-      <div key={index} className="card m-5 mx-auto" style={{ width: "300px" }}>
-        <img className="card-img-top" src={person.image} alt="Bob Dylan" />
+      <div
+        key={index}
+        className="card m-5 mx-auto col-md"
+        style={{ width: "300px" }}
+      >
+        <img
+          className="card-img-top"
+          src={person.image}
+          alt="Bob Dylan"
+          style={{
+            height: "40vh",
+            objectFit: "cover",
+            objectPosition: "top",
+          }}
+        />
         <div className="card-body">
           <h5 className="card-title">{person.cardTitle}</h5>
           <p className="card-text">{person.cardDescription}</p>
@@ -16,7 +29,7 @@ function App() {
       </div>
     );
   });
-  return <div>{infoSection}</div>;
+  return <div className="row">{infoSection}</div>;
 }
 
 export default App;
